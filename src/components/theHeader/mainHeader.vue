733ad94544bec7835ab0140669b6bbf882dd0aa7
mickquinton <jmgquinton@gmail.com>
2021-03-30 17:00:10 +0800

    Start project

---
+++ src/components/theHeader/mainHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+      <div>Logo</div>
+      <div>Nav</div>
+      <div>User Toggle</div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <main-header></main-header>
+  <section></section>
+</template>
+
+<script>
+import mainHeader from "../components/theHeader/mainHeader.vue"
+
+export default {
+  name: 'Home',
+
+  components: {
+    "main-header": mainHeader,
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
